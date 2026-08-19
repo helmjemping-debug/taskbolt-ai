59f161c60812e66b392ec2396e8c3903d282f3aa
helmjemping-debug <helmjemping@gmail.com>
2026-08-19 23:11:27 +0700

    Update model Groq ke llama-3.3-70b-specdec

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/api/chat', async (req, res) => {
     try {
         const chatCompletion = await groq.chat.completions.create({
             messages: [{ role: 'user', content: userText }],
-            model: 'llama-3.1-70b-versatile',
+            model: 'llama-3.3-70b-specdec',
         });
 
         res.json({ reply: chatCompletion.choices[0]?.message?.content || "Tidak ada respon." });
